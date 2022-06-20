4d96531714f1aa401dff04cb9af56006ab80d774
Juan Pablo Mancera <juanpablomanceramartinez@gmail.com>
2022-06-20 14:16:23 +0200

    First usable version

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify, current_app
 from flask_cors import CORS
-from flask_jwt_extended import create_access_token, JWTManager
+from flask_jwt_extended import create_access_token, JWTManager, jwt_required
 from screener.screener import results
 import os
 from dotenv import load_dotenv
@@ -15,19 +15,8 @@ secret_key = os.getenv("JWT_SECRET_KEY")
 app.config["JWT_SECRET_KEY"] = secret_key
 
 
-@app.route("/token", methods=["POST"])
-def create_token():
-    req_data = request.get_json()
-    email = req_data["email"]
-    password = req_data["password"]
-    if email != "test" or password != "test":
-        return jsonify({"msg":"Bad username or password"}), 401
-
-    acces_token = create_access_token(identity=email)
-    return jsonify(acces_token=acces_token)
-
-
 @app.route("/screener", methods=["POST"])
+@jwt_required()
 def filter_stocks():
     req_data = request.get_json()
     stocks = results(req_data)
@@ -64,28 +53,6 @@ def login():
     else:
         return jsonify({"msg":"User not found"})
 
-@app.route("/checkUser", methods=["POST","GET"])
-def checkUser(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        token = None
-        # jwt is passed in the request header
-        if 'x-access-token' in request.headers:
-            token = request.headers['x-access-token']
-        # return 401 if token is not passed
-        if not token:
-            return jsonify({'message': 'Token is missing !!'}), 401
-
-        try:
-            # decoding the payload to fetch the stored details
-            data = jwt.decode(token, app.config['SECRET_KEY'])
-
-        except:
-            return jsonify({
-                'message': 'Token is invalid !!'
-            }), 401
-        # returns the current logged in users contex to the routes
-        return jsonify({"msg":"Token valid"})
 
 if __name__ == "__main__":
     app.run(debug=True)
